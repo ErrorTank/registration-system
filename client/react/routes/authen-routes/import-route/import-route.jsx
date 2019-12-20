da7fd8cb786b3110bf4a05931cb291e94af960dd
Tuan Anh <ncq998@gmail.com>
2019-12-20 19:40:49 +0700

    dasdasdas

--- client/react/routes/authen-routes/import-route/import-route.jsx
+++ client/react/routes/authen-routes/import-route/import-route.jsx
@@ -1,14 +1,57 @@
 import React from "react";
 import {PageTitle} from "../../../common/page-title/page-title";
 import {AuthenLayout} from "../../../layout/authen-layout/authen-layout";
+import {DataTypePicker} from "./import-steps/data-type-picker";
+import {UploadExcel} from "./import-steps/upload-excel";
+import {ReviewData} from "./import-steps/review-data";
+import {MultipleSteps} from "../../../common/multiple-steps/multiple-steps";
 
 export default class ImportRoute extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.initData = {
+            dataType: 0,
+            currentStep: 0
+        };
+        this.state = {
+            ...this.initData
+        };
     };
 
+    handleImportData = () => {
+
+    };
+
+    steps = [
+        {
+            title: "Tải lên tệp dữ liệu",
+            subtitle: "Tải dữ liệu lên",
+            render: () => (
+                <UploadExcel
+
+                />
+            ),
+            canNext: () => true,
+            onNext: () => this.setState({currentStep: 1}),
+            onClickNav: () => this.setState({...this.initData}),
+            hideCancel: () => true
+        },{
+            title: "Xác nhận thông tin dữ liệu tải lên",
+            subtitle: "Xác nhận dữ liệu",
+            render: () => (
+                <ReviewData/>
+            ),
+            canNext: () => false,
+            onNext: this.handleImportData,
+            onPrevious: () => {
+                this.setState({...this.initData});
+            },
+            hideCancel: () => true
+        },
+    ];
+
     render() {
+        let {currentStep} = this.state;
         return (
             <PageTitle
                 title={"Import"}
@@ -17,6 +60,21 @@ export default class ImportRoute extends React.Component {
                     title={"Import dữ liệu"}
                 >
                     <div className="import-route">
+                        <div className="multiple-steps-wrapper">
+                            <MultipleSteps
+                                btnConfig={{
+                                    nextText: "Tiếp theo",
+                                    cancelText: "Hủy bỏ",
+                                    finishText: "Import dữ liệu",
+                                    previousText: "Trở về"
+                                }}
+                                curStepIndex={currentStep}
+                                steps={this.steps}
+                                onCancel={() => {
+                                    this.setState({...this.initData})
+                                }}
+                            />
+                        </div>
 
                     </div>
 
---
+++ client/react/routes/authen-routes/import-route/import-steps/data-type-picker.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export class DataTypePicker extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+        };
+    };
+
+
+
+    render(){
+        return(
+            <div className="data-type-picker">
+                <div className="option">
+
+                </div>
+                <div className="separate">
+                </div>
+                <div className="option">
+
+                </div>
+            </div>
+        );
+    }
+}
